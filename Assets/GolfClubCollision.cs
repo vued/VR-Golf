da1c89f7d9f19c0608849c84a361d1948e921516
David Möller <moelldav@gmail.com>
2017-06-20 21:56:30 +0200

    MVP
    it works - kind of

---
+++ Assets/GolfClubCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfClubCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnCollisionEnter(Collision other) {
+		Debug.Log (other.gameObject.tag);
+		// how much the character should be knocked back
+		var magnitude = 5000;
+		// calculate force vector
+		var force = transform.position - other.transform.position;
+		// normalize force vector to get direction only and trim magnitude
+		force.Normalize();
+		other.gameObject.GetComponent<Rigidbody>().AddForce(force * magnitude);
+	}
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision c)
+	{
+		// force is how forcefully we will push the player away from the enemy.
+		float force = 3000;
+
+		// Calculate Angle Between the collision point and the player
+		Vector3 dir = c.contacts[0].point - transform.position;
+		// We then get the opposite (-Vector3) and normalize it
+		dir = -dir.normalized;
+		// And finally we add force in the direction of dir and multiply it by force. 
+		// This will push back the player
+		c.contacts[0].otherCollider.attachedRigidbody.AddForce(dir*force);
+		c.contacts[0].thisCollider.attachedRigidbody.AddForce(dir*force);
+		//GetComponent<Rigidbody>().AddForce(dir*force);
+	}
+
+}
